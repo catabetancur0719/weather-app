78662c27dd95ecbbd4be21b10fde0a914b6e6e32
catabetancur0719 <catalina.betancur0719@gmail.com>
2025-07-07 19:52:57 -0500

    fixed footer, changed some css and added search engine

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,51 +1,10 @@
-function handleSearch(event) {
-  event.preventDefault(); //avoid to reload the page
-
-  const searchInputElement = document.querySelector(".searchInput");
-  const cityElement = document.querySelector(".titleCity");
-  cityElement.innerHTML = searchInputElement.value;
-
+function handleSearchSubmit(event){
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector(".titleCity");
+  cityElement.innerHTML = searchInput.value
 }
 
-function formatDate(date){
-
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let day = date.getDay();
-  //console.log(minutes)
-
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-
-  const weekDays = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-
-  const formatDay = weekDays[day];
-  return `${formatDay} ${hours}:${minutes}`;
-}
-
-
-const searchBar = document.querySelector(".searchForm");
-searchBar.addEventListener("submit", handleSearch);
-
-const currentDateElement = document.querySelector(".dayInformation");
-const currentDate = new Date()
-console.log(currentDateElement);
-currentDateElement.innerHTML = formatDate(currentDate);
-
-
-
-
+let searchFormElement = document.querySelector(".searchForm");
+//console.log(searchFormElement);
+searchFormElement.addEventListener("submit", handleSearchSubmit)
